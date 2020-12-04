bb4e1e044021d7ed077ec86f0a021dbb82730f69
Benjamin Cash <ben@benjamincash.com>
2020-12-03 21:16:44 -0700

    Converting to classes, isolating services

--- week7/public/auth/auth.service.js
+++ week7/public/auth/auth.service.js
@@ -1,11 +1,93 @@
-const AUTH_API = `${BASE_URL}/auth`;
+/**
+ * @class AuthService
+ * Service for the auth component
+ */
+class AuthService {
+    authAPIService;
 
-const requestRegister = (data) => _post(`${AUTH_API}/register`, data);
+    constructor(authAPIService) {
+        this.authAPIService = authAPIService;
+    }
 
-const requestLogin = (data) => _post(`${AUTH_API}/login`, data);
+    /**
+     * Logs a user in.
+     * @param {Object} event 
+     */
+    _login = async (e) => {
+        e.preventDefault();
+        const username = document.getElementById('usernameLogin').value;
+        const password = document.getElementById('passwordLogin').value;
+        
+        try {
+            const res = await authAPIService.login({ username, password });
+            if(res.success) {
+                setStorage('access_token', res.access_token);
+                setStorage('refresh_token', res.refresh_token);
+                setStorage('logged_in', true);
+                alert(res.message);
+                window.location.href = '/item/item.html';
+            } else {
+                alert(res.error);
+            }
+        }
+        catch(err) {
+            console.error(err);
+        }
+    }
 
-const requestLogout = () => {
-    clearStorage('logged_in');
-    clearStorage('access_token');
-    clearStorage('refresh_token');
+    /**
+     * Registers a new user.
+     * @param {Object} event 
+     */
+    _register = async (e) => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const username = document.getElementById('usernameRegister').value;
+        const password = document.getElementById('passwordRegister').value;
+    
+        try {
+            const res = await authAPIService.register({ name, email, username, password });
+            if(res.success) {
+                console.log(res);
+                alert(res.message);
+                window.location.href = '/';
+            } else {
+                console.log(res);
+                alert(res.error);
+            }
+        }
+        catch(err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * Logs a user out.
+     * @param {Object} event 
+     */
+    _logout = async (e) => {
+        e.preventDefault();
+        try {
+            await authAPIService.logout();
+            window.location.href = '/';
+        }
+        catch(err) {
+            console.error(err);
+        }
+    }
+
+    /**
+     * Checks if a user is logged in.
+     */
+    _isLoggedIn = () => {
+        return storageHasData() ? getStorage("logged_in") : false;
+    }
 }
+
+const authService = new AuthService(authAPIService);
+
+
+
+
+
--- week7/public/lib/route-guard.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    const loggedIn = storageHasData() ? getStorage("logged_in") : false;
-    if(!loggedIn) {
-        logout();
-    }
-})();
