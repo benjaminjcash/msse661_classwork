d0d448b54480683dda6f82b9ae2f34002fc03e09
Benjamin Cash <ben@benjamincash.com>
2020-12-12 13:05:27 -0700

    Adding cert, configuring server to use https

--- week8/server/index.js
+++ week8/server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const PORT = process.env.PORT || 4000;
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const PORT = process.env.PORT || 4443;
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(PORT, function() {
+https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'server.key')),
+    cert: fs.readFileSync(path.join(__dirname, './server.cert'))
+}, app).listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}...`);
 });
